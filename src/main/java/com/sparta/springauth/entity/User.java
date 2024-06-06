79b36a6bef99b390c3bcbca4c2c0024c0af97195
leesewon <leesw1945@gmail.com>
2024-06-07 01:04:00 +0900

    회원가입 구현

---
+++ src/main/java/com/sparta/springauth/entity/User.java
@@ -0,0 +1,38 @@
+package com.sparta.springauth.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    // enum 타입을 데이터베이스 컬럼에 저장할 때 사용하는 어노테이션
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
